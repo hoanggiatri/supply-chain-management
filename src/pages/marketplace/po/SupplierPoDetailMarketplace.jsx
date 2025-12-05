00b65a2c2b3245bd449a3b88eb1229c480a3f7d9
HGT <hoangtri21062003@gmail.com>
2025-12-05 21:56:51 +0700

    fix: delete icon

--- src/pages/marketplace/po/SupplierPoDetailMarketplace.jsx
+++ src/pages/marketplace/po/SupplierPoDetailMarketplace.jsx
@@ -162,20 +162,6 @@ const SupplierPoDetailMarketplace = () => {
               )}
               {po.status === "Đã xác nhận" && (
                 <Button {...getButtonProps("primary")} onClick={handleCreateSo}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    className="w-5 h-5 mr-2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 4.5v15m7.5-7.5h-15"
-                    />
-                  </svg>
                   Tạo đơn bán hàng
                 </Button>
               )}
@@ -281,7 +267,3 @@ const SupplierPoDetailMarketplace = () => {
 };
 
 export default SupplierPoDetailMarketplace;
-
-
-
-
--- src/pages/marketplace/quotation/CustomerQuotationDetailMarketplace.jsx
+++ src/pages/marketplace/quotation/CustomerQuotationDetailMarketplace.jsx
@@ -169,57 +169,15 @@ const CustomerQuotationDetailMarketplace = () => {
               {quotation.status === "Đã báo giá" && (
                 <>
                   <Button {...getButtonProps("success")} onClick={handleAccept}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={2}
-                      stroke="currentColor"
-                      className="w-5 h-5 mr-2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m4.5 12.75 6 6 9-13.5"
-                      />
-                    </svg>
                     Chấp nhận
                   </Button>
                   <Button {...getButtonProps("danger")} onClick={handleReject}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={2}
-                      stroke="currentColor"
-                      className="w-5 h-5 mr-2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18 18 6M6 6l12 12"
-                      />
-                    </svg>
                     Từ chối
                   </Button>
                 </>
               )}
               {quotation.status === "Đã chấp nhận" && (
                 <Button {...getButtonProps("primary")} onClick={handleCreatePo}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    className="w-5 h-5 mr-2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 4.5v15m7.5-7.5h-15"
-                    />
-                  </svg>
                   Tạo đơn mua hàng
                 </Button>
               )}
