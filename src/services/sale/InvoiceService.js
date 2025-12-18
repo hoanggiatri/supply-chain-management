485d9fa4986637047a65f4634eaff6981ae8a7aa
Sotatek-MinhNguyen25 <minh.nguyen25@sotatek.com>
2025-12-18 14:46:03 +0700

    update invoice

--- src/services/sale/InvoiceService.js
+++ src/services/sale/InvoiceService.js
@@ -2,22 +2,17 @@ import axios from "axios";
 import { API_BASE_URL as BASE_URL } from "../../config/api";
 
 export const getInvoicePdf = async (soId, token) => {
-  const response = await axios.post(`${BASE_URL}/invoices/sales-orders/${soId}`, {}, {
-    headers: {
-      Authorization: `Bearer ${token}`
+  const response = await axios.get(
+    `${BASE_URL}/invoices/sales-orders/${soId}`,
+    {
+      responseType: "blob",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
-  });
+  );
 
-  if (response.data && response.data.file) {
-    const base64Data = response.data.file;
-    const byteCharacters = atob(base64Data);
-    const byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++) {
-      byteNumbers[i] = byteCharacters.charCodeAt(i);
-    }
-    const byteArray = new Uint8Array(byteNumbers);
-    const blob = new Blob([byteArray], { type: "application/pdf" });
-    const url = window.URL.createObjectURL(blob);
-    window.open(url);
-  }
+  const blob = new Blob([response.data], { type: "application/pdf" });
+  const url = window.URL.createObjectURL(blob);
+  window.open(url);
 };
