e02f79de1d10d9c5719b2decea3caa091c465dac
HGT <hoangtri21062003@gmail.com>
2025-12-20 03:05:58 +0700

    trigger

--- src/pages/purchasing/rfq/RfqInCompany.jsx
+++ src/pages/purchasing/rfq/RfqInCompany.jsx
@@ -2,7 +2,7 @@ import { AddButton } from "@/components/common/ActionButtons";
 import ListPageLayout from "@/components/layout/ListPageLayout";
 import { DataTable, createSortableHeader } from "@/components/ui/data-table";
 import { StatusSummaryCard } from "@/components/ui/status-summary-card";
-import { getAllRfqsInRequestedCompany } from "@/services/purchasing/RfqService";
+import { getAllRfqsInCompany } from "@/services/purchasing/RfqService";
 import toastrService from "@/services/toastrService";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -20,7 +20,7 @@ const RfqInCompany = () => {
     const fetchRfqs = async () => {
       setLoading(true);
       try {
-        const data = await getAllRfqsInRequestedCompany(companyId, token);
+        const data = await getAllRfqsInCompany(companyId, token);
         setRfqs(data);
       } catch (error) {
         toastrService.error(
